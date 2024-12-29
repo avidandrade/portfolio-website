f1b5069596b5be8ee2ea0e97daa58515937720d2
avidandrade <trejoavid@gmail.com>
2024-12-29 17:44:38 -0500

    Added links to my projects.

--- src/Projectitem.jsx
+++ src/Projectitem.jsx
@@ -22,7 +22,9 @@ function Project() {
                     initial = {{opacity: 0, x: -100}}
                     transition = {{duration: 1.4}}
                     className="btn-container">
-                    <img className="img-project" src={Project2} alt="" />
+                    <a href="https://github.com/avidandrade/SE2-Project" target ="_blank" >
+                        <img className="img-project" src={Project2} alt="" />
+                    </a>
                     <h7>Full Stack Task Manager App</h7>
                     <p>Led a team of four to develop a scalable task management app using Agile methodology, with a Node.js/Express backend, React/TypeScript frontend, and MySQL database. Designed seven reusable API routes for task and file CRUD operations, ensuring robust validation and error handling for a smooth user experience.</p>
                 
@@ -33,7 +35,9 @@ function Project() {
                     initial = {{opacity: 0, x: -100}}
                     transition = {{duration: 1.2}}
                     className="btn-container">
-                    <img className="img-project" src={Project1} alt="" />
+                    <a href="https://github.com/avidandrade/Online-Bookstore-API" target="_blank">
+                        <img className="img-project" src={Project1} alt="" />
+                    </a>
                     <h7>RESTFUL API Bookstore</h7>
                     <p>Developed a Online bookstore using Spring Boot as the backend and PostgreSQL as the database. Developed Book browsing and sorting features to the API. The project has 20 endpoints.</p>
                 </motion.div>
@@ -43,7 +47,9 @@ function Project() {
                     initial = {{opacity: 0, x: -100}}
                     transition = {{duration: 1.6}}
                     className="btn-container">
-                    <img className="img-project" src={Project3} alt="" />
+                    <a>
+                        <img className="img-project" src={Project3} alt="" />
+                    </a>
                     <h7>Resume AutoFill Extension</h7>
                     <p>Create a platform for hosting virtual events, conferences, and workshops. Include features such as live streaming, interactive Q&A sessions, networking opportunities, virtual booths, and attendee engagement tools.</p>
                     
