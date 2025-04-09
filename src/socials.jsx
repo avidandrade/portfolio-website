6f1d141dc9d1fed44d404b4b30796ef21b44ca9d
avidandrade <trejoavid@gmail.com>
2025-04-08 23:32:56 -0400

    Updated social section with my github.

--- src/socials.jsx
+++ src/socials.jsx
@@ -22,11 +22,10 @@ function Socials() {
 
                 <h4> Feel Free to Contact Me!</h4>
                 <div className="socials">
-                    <motion.a href="https://www.instagram.com/hmavid_/" variants = {iconVariants(1.2)} initial= "initial" animate = "animate" target="_blank"><i className='bx bxl-instagram'></i></motion.a>
+                    <motion.a href="https://github.com/avidandrade" variants = {iconVariants(1.2)} initial= "initial" animate = "animate" target="_blank"><i className='bx bxl-github'></i></motion.a>
                     <motion.a href="https://www.linkedin.com/in/avid-andrade-trejo-898a87251/" variants = {iconVariants(1.3)} initial= "initial" animate = "animate" target="_blank"> <i
                         className='bx bxl-linkedin'></i>
                     </motion.a>
-                    <motion.a href="https://www.youtube.com" variants = {iconVariants(1.1)} initial= "initial" animate = "animate" > <i target ="_blank"className='bx bxl-youtube'></i> </motion.a>
                 </div>
             </div>
 
